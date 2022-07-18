21552dc89b4969e9e1307bea1168c5c79106aae2
Malware <kueprotocol11@gmail.com>
2022-07-18 14:16:10 +0000

    added nww..cpp to the directoory

---
+++ nww..cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+#include <climits>
+
+using namespace std;
+using std::cin;
+using std::cout;
+using std::string;
+
+int main()
+{
+  
+  cout<<"1 line"<<endl;
+   cout<<"1 line"<<endl;
+    cout<<"1 line"<<endl;
+     cout<<"1 line"<<endl;
+      cout<<"1 line"<<endl;
+       cout<<"1 line"<<endl;
+        cout<<"1 line"<<endl;
+         cout<<"1 line"<<endl;
+          cout<<"1 line"<<endl;
+           cout<<"1 line"<<endl;
+            cout<<"1 line"<<endl;
+             cout<<"1 line"<<endl;
+              cout<<"1 line"<<endl;
+
+
+
+ }
